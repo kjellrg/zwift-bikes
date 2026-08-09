85964af123c3f1d5db99c993344cf77a783bf73d
Kjell Rune Glaerum <24758856+kjellrg@users.noreply.github.com>
2026-08-09 21:39:02 +0200

    Export physics engine primitives

---
+++ shared/utils/physics/index.ts
@@ -0,0 +1,4 @@
+export { calculateForces, rollingResistanceCoefficient, GRAVITY, AIR_DENSITY, DRIVETRAIN_EFFICIENCY } from './forces'
+export { bikeMassFromScore, cdaFromScore, equipmentPhysics } from './equipment'
+export { geometryFromRoute, simulateRoute } from './simulator'
+export type { SimulateRouteOptions } from './simulator'
